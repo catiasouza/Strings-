05819bbb58f1173384fba68e3e7f1b127f85ad78
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-09-30 14:25:36 -0300

    Quebra de linhas

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,6 +3,26 @@ import UIKit
 var texto = "Vamos comecar a estudar as novidades de Swift!!!"
 print(texto.filter({ $0 != "!" }))
 
-let listaNomes = ["Andre","Paulo","Carla","Jorge"]
-listaNomes.forEach({print($0)})
-texto.forEach({print( $0 )})
+//let listaNomes = ["Andre","Paulo","Carla","Jorge"]
+//listaNomes.forEach({print($0)})
+//texto.forEach({print( $0 )})
+
+let nomes = ["Catia","Fatima","Paulo","Carla","Jorge"]
+print(nomes.count)
+
+let mensagem = "A linguagem Swift é moderna e intuitiva. \nEstamos aprendendo as principais novidades. \nComecamos com as novidades da API de dicionarios. \nE nesta aula estamos treinando Strings."
+//print(mensagem.count)
+//for caracter in mensagem{
+//    print(caracter)
+//}
+print(mensagem)
+print()
+//com """ podemos formatar string e texto
+let novaMensagem = """
+"A linguagem Swift é moderna e intuitiva.
+Estamos aprendendo as principais novidades.
+    Comecamos com as novidades da API de dicionarios.
+E nesta aula estamos treinando
+Strings."
+"""
+print(novaMensagem)
